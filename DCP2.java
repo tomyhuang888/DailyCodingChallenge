d6732315e2caec289719fa15f12a5acd22ca6b5e
Thomas Huang <thomasyh@uci.edu>
2019-02-11 21:37:11 -1000

    Including Documentation to DCP2.java

--- DCP2.java
+++ DCP2.java
@@ -1,5 +1,36 @@
+/**
+* Daily Coding Problem: Problem #2
+* 
+* This problem was asked by Uber.
+* 
+* Given an array of integers, return a new array such 
+* that each element at index i of the new array is the 
+* product of all the numbers in the original array except 
+* the one at i.
+* 
+* For example, if our input was [1, 2, 3, 4, 5], the 
+* expected output would be [120, 60, 40, 30, 24]. If our 
+* input was [3, 2, 1], the expected output would be [2, 3, 6].
+*
+* Follow-up: what if you can't use division?
+*/
+
 public class DCP2{
 
+	/**
+	 * Divide method
+	 * 
+	 * Multiplying all the elements in the original array then 
+	 * at individually dividing that number by each element at index
+	 * i to create a new int array where each element at index i is 
+	 * the product of all the numbers in the original array except the 
+	 * one at i.
+	 *
+	 * @param	arr list of integers
+	 * @return	a new array where each element at index i is the 
+	 * 			product of all the numbers in the original array
+	 *			except the one at i.
+	 */
 	public static int[] divide(int[] arr){
 		int total = 1;
 		for(int i = 0; i < arr.length; i++){
@@ -14,13 +45,26 @@ public class DCP2{
 		return newArr;
 	}
 
+	/**
+	 * Without Using Divide Method
+	 * 
+	 * Using two list traversals, one starting from the left and 
+	 * one starting from the right to create two list, the left list 
+	 * consists of elements which are the product of the elements to 
+	 * the left of the index and the right list consists of elements 
+	 * which are the product of the elements to the right of the index.
+	 *
+	 * @param	arr list of integers
+	 * @return	a new array where each element at index i is the 
+	 * 			product of all the numbers in the original array
+	 *			except the one at i.
+	 */
 	public static int[] noDivide(int[] arr){
 		int[] leftArr = new int[arr.length];
 		int[] rightArr = new int[arr.length];
 
 		leftArr[0] = 1;
 		for(int i = 1 ; i < arr.length; i++){
-			//leftArr[i] = 1;
 			leftArr[i] = leftArr[i-1] * arr[i-1];
 		}
 		rightArr[arr.length-1] = 1;
